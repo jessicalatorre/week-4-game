fb0a5f447abeee34e00d91ee4d4486419aff28dc
Jessica La Torre <jess.latorre@gmail.com>
2018-03-18 20:29:03 -0500

    added new variables and for loops to game.js file. Will use if statements to add attriubtes to nidex file when jQuery runs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,59 @@
-// Note that this div is now empty. We will fill it dynamically -->
+//Psuedo Code: We want the computer to randomly select a number between 19-120 that will be shown in browser at the start of each game. 
+//There will be 4 crystals and each will have a value that isn't displayed
+//players will click on each crystal to increase his/her score
+//When the player's score matches the computer's random number (score), the player wins 1 round, a "You won!" displays on browser, and the game resets.
+//If the player's score is greater than the computer's score, the player loses one round, display in browser shows "You lost, but keep trying!" and the game resets.
 
-  <script type="text/javascript">
 
-  var targetNumber = 53;
-  $("#number-to-guess").text(targetNumber);
-  var crystals = $("#crystals");
-  var counter = 0;
+//step one with jQuery: ready your document
+$(document).ready(() => {
+var computerNumber = 0;
+var number = 0;
+var numberOptions =[]; //everytime a crystal is pressed this will be add to the counter
 
-  // Now for the hard part. Creating multiple crystals each with their own unique number value.
-  // We begin by expanding our array to include four options.
-  var numberOptions = [10, 5, 3, 7];
+var randomNumber = function() {
+  return Math.floor(Math.random() * (121 - 19)) + 19; //highest number goes first
+}
+//when you call the function it must be outside of the orginal function and it must be assigned to different variable
+computerNumber = randomNumber();
+console.log(computerNumber);
 
+//add another function that randomly assigns numbers to each of the four crystals (number between 1-12);
+//use jQuery to assign
+//for loop and assign to each one of crystal variables 4 different times
+
+var crystalNumber = function() {
+  return Math.floor(Math.random() * (13 - 1)) + 1; //pay attention to numbers
+}
+  $("#number-to-guess").text(computerNumber); //use JQuery to assign number to crystal
   // Next we create a for loop to create crystals for every numberOption.
-  for (var i = 0; i < numberOptions.length; i++) {
 
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
+  for (var i = 0; i < 4; i++) { //we need to get 4 numbers to put them in an arry
+      number = crystalNumber(); // be sure to make number variable global the same way we did for computer Number var after it was reassigned from randomNumber.
+      numberOptions.push(number); //after we have the randomly created number and we push it into the number options array. This will happen each time the for loop runs.
+      console.log(numberOptions[i]); //be sure to include the index and close the for loop
+    }
+   
     // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    for (var i = 0; i < numberOptions.length; i++) { // since this is repetitive cycle until we use all the numbers in the array, we create another for loop to assign the number to the crystals
+      var imageCrystal = $("<img>"); //using Jquery to add an image tag and assigning it to newly created imageCrystal
+      imageCrystal.addClass("crystal-image"); //added class call crystal 
 
+      if( i ===blue){
+        imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg")
+
+      } else if (i ===green){
+        imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg")
+
+      } else if(i === red){
+        imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg")
+
+      } else if (i===yellow){
+        imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg")
+      }
+    );
+    
+  
     // Each imageCrystal will be given a data attribute called data-crystalValue.
     // This data attribute will be set equal to the array value.
 
@@ -31,7 +61,6 @@
     // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
 
     crystals.append(imageCrystal);
-  }
   // This time, our click event applies to every single crystal on the page. Not just one.
   crystals.on("click", ".crystal-image", function() {
 
@@ -55,5 +84,5 @@
       alert("You lose!!");
     }
   });
-  
-  </script>
+
+// });
